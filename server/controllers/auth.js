e5de5a4ec8783b0c606c4ec46a2a4c89909ae465
Ono-Saito <onosaito0614@gmail.com>
2021-04-15 19:19:03 +0500

    feat(route): add login route and credentials

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -20,8 +20,29 @@ exports.register = async (req, res, next) => {
   }
 };
 
-exports.login = (req, res, next) => {
-  res.send('Login Route');
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res
+      .status(400)
+      .json({ success: false, error: 'Please provide an email and password' });
+  }
+  try {
+    const user = await User.findOne({ email }).select('+password');
+    if (!user) {
+      res.status(404).json({ success: false, error: 'Invalid credentials' });
+    }
+    const isMatch = await user.matchPasswords(password);
+    if (!isMatch) {
+      res.status(404).json({
+        success: false,
+        error: 'Invalid credentials',
+      });
+    }
+    res.status(200).json({ success: true, token: 'token' });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
 };
 
 exports.forgotPassword = (req, res, next) => {
--- server/models/User.js
+++ server/models/User.js
@@ -28,11 +28,16 @@ const UserSchema = new mongoose.Schema({
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
+
+UserSchema.methods.matchPasswords = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model('User', UserSchema);
 
